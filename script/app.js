bd98c808ef8d95efd7187ceac181a8f2dfa0c1e2
Damian1255 <damianlau96@gmail.com>
2023-03-24 18:00:59 +0800

    bold text

--- script/app.js
+++ script/app.js
@@ -5,6 +5,7 @@ output = document.getElementById("output");
 
 submit_btn.addEventListener("click", function () {
     submit_btn.disabled = true;
+    output.style.display = "none";
     loading.style.display = "block";
     message = document.getElementById("input").value;
     
@@ -27,9 +28,9 @@ submit_btn.addEventListener("click", function () {
         confidence = data.confidence.toFixed(2);;
 
         if (prediction == 0) {
-            output.innerHTML = "<p>Your message is " + confidence + "% <b>unlikely</b> a Spam.</p>";
+            output.innerHTML = "<p>Your message is <b>" + confidence + "% unlikely a Spam.</b></p>";
         } else {
-            output.innerHTML = "<p>Your message is " + confidence + "% <b>likely</b> a Spam.</p>";
+            output.innerHTML = "<p>Your message is <b>" + confidence + "% likely a Spam.</b></p>";
         }
     });
 });
